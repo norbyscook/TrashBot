d684ca560c2fcee885cabfe2532903c5af949a1e
norbyscook <andyqe@gmail.com>
2021-08-31 10:57:02 -0700

    init

---
+++ TrashRobot/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+int main()
+{
+	cout << "Hello! I am trash collector bot! <insert track collector bot ascii art>\n"
+		<< "please enter the number of trash you cleaned up today!\n";
+
+
+}
+
+double get_dobule()
+{
+	int input = { 0 };
+	while (!(cin >> input))
+	{
+		break;
+	}
+	return 0;
+}
