c7ea1b27586828b61594541956598a7cf66e7790
norbyscook <andyqe@gmail.com>
2021-09-06 11:41:53 -0700

    moved function definition from AchievementClass.cpp to AchievementClass.h

--- TrashRobot/AchievementClass.h
+++ TrashRobot/AchievementClass.h
@@ -50,21 +50,73 @@ private:
 	// update status functions ----------------------------------------
 
 	// set achievement status if requirement is met
-	void set_achievement(achieve&, uint32_t);
+	void set_achievement(achieve& element, uint32_t score)
+	{
+		if (score >= element.score_requirement)
+		{
+			cout << "\n!!! you have obtained a new achievement !!!\n\n";
+			element.status = true;
+		}
+	}
 
 	// file write functions  ----------------------------------------
 
-	void write_to_file(ostream& out_file);
+	void write_to_file(ostream& out_file)
+	{
+		out_file << "score, path, status\n";
+		for (auto element : loaded_achievements)
+		{
+			out_file << element.score_requirement << ",";
+			out_file << element.file_path << ",";
+			out_file << element.status << "\n";
+		}
+	}
 
 public:
 	// loads achievement from file into vector
-	void load(string);
+	void load(string file_path)
+	{
+		ifstream in_file(file_path);
+		if (!in_file) { cout << "I can't find achievement log file! in" << file_path << "\n"; }
+		else { loaded_achievements = extract_line(in_file); }
+		in_file.close();
+	}
 	// update status of achievement in vector
-	void update_status(uint32_t score);
+	void update_status(uint32_t score)
+	{
+		for (achieve& element : loaded_achievements)
+		{
+			if (element.status == false) { set_achievement(element, score); }
+		}
+	}
+
 	// get obtained achievements
-	string get_obtained();
+	string get_obtained()
+	{
+		string obtained = { "" };
+		for (auto element : loaded_achievements)
+		{
+			if (element.status == true)
+			{
+				obtained += get_ascii_art(element.file_path) + "\n\n";
+			}
+		}
+		if (obtained.size() < 1)
+		{
+			cout << "no achievement yet! we need more scores!\n\n";
+		}
+		return obtained;
+	}
+
 	// write updated achievements to file
-	void update_file(string);
+	void update_file(string file_path)
+	{
+		ofstream out_file(file_path);
+		string line = { "" };
+		if (!out_file) { cout << "can't find " << file_path << "\n"; }
+		else { write_to_file(out_file); }
+		out_file.close();
+	}
 
 
 };
