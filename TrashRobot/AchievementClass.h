9c9f0df1fcda6a27f08bff50fc71afa02bb78da2
norbyscook <andyqe@gmail.com>
2021-09-06 12:03:49 -0700

    fix merge errors

--- TrashRobot/AchievementClass.h
+++ TrashRobot/AchievementClass.h
@@ -18,30 +18,9 @@ private:
 	vector<achieve> loaded_achievements;
 
 	// extract a line of the csv file
-	vector<achieve> extract_line(istream& in_file)
-	{
-		vector<achieve> vect;
-		string line = { "" };
-		getline(in_file, line);
-		while (getline(in_file, line))
-		{
-			extract_cell(line, vect);
-		}
-		return vect;
-	}
+	vector<achieve> extract_line(istream& in_file);
 	// extract cells of the line and place it into achievement vector
-	void extract_cell(std::string line, std::vector<achieve>& vect)
-	{
-		vector<string> line_elements(3);
-		stringstream line_stream(line);
-		string cell = { "" };
-		for (size_t i = 0; getline(line_stream, cell, ','); i++)
-		{
-			try { line_elements.at(i) = cell; }
-			catch (std::out_of_range oor) { cout << oor.what() << "\n"; }
-		}
-		vect.push_back({ stoul(line_elements[0]), line_elements[1], bool(stoul(line_elements[2])) });
-	}
+	void extract_cell(std::string line, std::vector<achieve>& vect);
 
 	// set achievement status if requirement is met
 	void set_achievement(achieve&, uint32_t);
